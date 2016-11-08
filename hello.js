8795d132a3f543963023dfd19ffa534edc384984
sindu <sindu0225@gmail.com>
2016-11-08 15:16:47 +0100

    add edit squash and fixup function
    add edit function
    
    add squash function
    
    add fixuph function

--- hello.js
+++ hello.js
@@ -1,1 +1,3 @@
-let new = () -> { console.log 'This is a new function 🙂' }
+let edit = () -> { console.log 'This is a editw function 🙂' }
+let squash = () -> { console.log 'This is a squash function 🙂' }
+let fixup = () -> { console.log 'This is a fixup function 🙂' }
