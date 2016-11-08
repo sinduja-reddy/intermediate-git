f74de1e18dec45df83956ddadc1bf3bd9abe1aa8
sindu <sindu0225@gmail.com>
2016-11-08 15:28:34 +0100

    add fixup function

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-let edit = () -> { console.log 'This is a editw function 🙂' }
-let squash = () -> { console.log 'This is a squash function 🙂' }
+let fixup1 = () -> { console.log 'This is a editw function 🙂' }
+let fixup2 = () -> { console.log 'This is a squash function 🙂' }
 let fixup = () -> { console.log 'This is a fixup function 🙂' }
